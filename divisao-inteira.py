1af439b7d67dbb4f723d7d7ab23ecd80a91ce031
narciso-roberto <narcisoroberto12@gmail.com>
2024-04-01 11:58:22 -0300

    Questoes

---
+++ divisao-inteira.py
@@ -0,0 +1,6 @@
+n1 = int(input())
+n2 = int(input())
+
+print(round(n1 / n2))
+print(n1 % n2)
+print(f"{n1/n2:.2f}")
---
+++ operaçoesBasicas.py
@@ -0,0 +1,8 @@
+n1 = int(input())
+n2 = int(input())
+
+print(n1+n2)
+print(n1-n2)
+print(n1*n2)
+print(f"{n1/n2:.2f}")
+print(n1%n2)
