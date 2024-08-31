d65d45dcc27c3df39ffb83c0b6bb6161b1605c06
narciso-roberto <narcisoroberto1369@gmail.com>
2024-08-31 18:58:32 -0300

    terminado banner

--- src/Componentes/Banner.jsx
+++ src/Componentes/Banner.jsx
@@ -1,32 +1,63 @@
 import React from 'react'
-
 import style from './banner.module.css'
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Banner = ({movies}) => {
 
   const [slide,setSlide] = React.useState(0);
+  const [fade,setFade] = React.useState(true);
 
-  const mudar = setInterval(() => {
-    if(slide+1 > 19){
-      setSlide(0)
-    }else{
-      setSlide(slide+1)
-    }
-    clearInterval(mudar)
-  },6000)
+  React.useEffect(() => {
+    const mudar = setInterval(() => {
+      setFade(false)
+      
+      // console.log("sumindo")
+      setTimeout(() => {
+        setFade(true)
+        slide<19 ? setSlide(slide+1) : setSlide(0)
+        },1000)
+      },6000)
+      return () => {clearInterval(mudar)}
+  },[slide])
+
+
+
+
 
   let movie = movies[slide];
-    
   return (
     <div className={style.banner}>
-        <img src={`https://image.tmdb.org/t/p/original/${movie['backdrop_path']}`}/>
+        <img
+        className={fade ? style.fadeIn :style.fadeOut}
+        src={`https://image.tmdb.org/t/p/original/${movie['backdrop_path']}`}
+        />
 
-        <div className={style.desc}>
+        
+        <div className={style.skipBack}>
+          <button className={style.bannerBtn} disabled={!fade} onClick={() => {
+            setFade(false)
+            setTimeout(() => {
+            setSlide(slide-1)
+            setFade(true)
+          },1000)
+          }}><IoIosArrowBack/></button>
+
+          <div className={`${style.desc} ${fade ? style.fadeIn :style.fadeOut}`}>
             <h1>{movie.original_title}</h1>
             <p>{movie.overview}</p>
             <button>COMPRAR</button>
+          </div>
+
+          <button className={style.bannerBtn} disabled={!fade} onClick={() => {
+            setFade(false)
+            setTimeout(() => {
+            setSlide(slide+1)
+            setFade(true)
+          },1000)
+          }}><IoIosArrowForward/></button>
+
         </div>
-        
     </div>
   )
 }
